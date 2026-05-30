f17e0f161099f60f033ed1c06e9ca2fae6d9e3b8
xKing <chaseguy.kawai21@gmail.com>
2026-05-29 21:00:46 -0500

    a

--- utdx/units/pirate_king.js
+++ utdx/units/pirate_king.js
@@ -23,8 +23,8 @@ unitDatabase.push({
 
     upgrades: [
         { dmg: 360, spa: 7, range: 24, cost: 0 },     // Up 0 (Base)
-        { dmg: 780, spa: 7, range: 25, cost: 5000 },  // Up 1
-        { dmg: 1350, spa: 7, range: 26, cost: 8000 }, // Up 2
+        { dmg: 780, spa: 7, range: 25, cost: 6000 },  // Up 1
+        { dmg: 1350, spa: 7, range: 26, cost: 9000 }, // Up 2
         { dmg: 2000, spa: 8.5, range: 28, cost: 13000 }, // Up 3
         { dmg: 4400, spa: 12, range: 30, cost: 18000 }, // Up 4
         { dmg: 5000, spa: 12, range: 31, cost: 25000 }, // Up 5
