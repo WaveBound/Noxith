b6ec207c2d66826c9c549f43f683be2186ec96df
xKing <chaseguy.kawai21@gmail.com>
2026-08-14 07:25:03 -0500

    a

--- AE/data/units/carrotunleashed.js
+++ AE/data/units/carrotunleashed.js
@@ -7,7 +7,7 @@ export const unit = {
     archetype: "Physical",
 
     stats: {
-        recommendedTrait: "Forsaken",
+        recommendedTrait: "Primordial",
         element: "Gale",
         archetype: "Physical",
         damage: "2,572",
